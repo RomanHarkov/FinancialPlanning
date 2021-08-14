88a948e594ce7e1a6729031edb820496c583b0b7
Роман <harkov_rv@mail.ru>
2021-08-14 16:41:52 +0300

    gg

---
+++ src/Entities/Indicator.java
@@ -0,0 +1,17 @@
+package Entities;
+
+/**
+ * Created by Роман on 06.07.2021.
+ */
+public class Indicator {
+
+    String name;
+    Section section;
+
+    public Indicator(Section section, String name){
+
+        this.section = section;
+        this.name = name;
+
+    }
+}
